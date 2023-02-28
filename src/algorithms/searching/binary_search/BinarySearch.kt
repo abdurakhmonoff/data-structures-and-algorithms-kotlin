25d27141442e16c4dc5894b11a0ac51743f97d0a
Asror <a.abdurakhmonoff@gmail.com>
2023-02-28 13:58:28 +0500

    Add comments to search algorithms functions for improved code clarity and understanding

--- src/algorithms/searching/binary_search/BinarySearch.kt
+++ src/algorithms/searching/binary_search/BinarySearch.kt
@@ -1,9 +1,14 @@
 package algorithms.searching.binary_search
 
-/**
- * Binary search only works with only sorted arrays
- */
 class BinarySearch {
+
+    /**
+     * Performs a binary search on an array of integers to find the index of a specified key.
+     *
+     * @param arr the array to search
+     * @param key the value to search for
+     * @return a message indicating whether the element was found and its index, or that it was not found
+     */
     fun binarySearch(arr: IntArray, key: Int): String {
         var first = 0
         var last = arr.size - 1
@@ -14,7 +19,7 @@ class BinarySearch {
                     first = middle + 1
                 }
                 arr[middle] == key -> {
-                    return "Element Found At Index $middle"
+                    return "Element found at index $middle"
                 }
                 else -> {
                     last = middle - 1
@@ -22,15 +27,19 @@ class BinarySearch {
             }
             middle = (first + last) / 2
         }
-        return "Element Not Found"
+        return "Element not found!"
     }
 }
 
 fun main() {
+
     val binarySearch = BinarySearch()
+
+    // Note: Binary search only works with only sorted arrays
     val array = intArrayOf(10, 25, 32, 45, 55, 68)
     val result = binarySearch.binarySearch(array, 55)
     println(result)
+
     val result2 = binarySearch.binarySearch(array, 100)
     println(result2)
 }
--- src/algorithms/searching/linear_search/LinearSearch.kt
+++ src/algorithms/searching/linear_search/LinearSearch.kt
@@ -1,21 +1,28 @@
 package algorithms.searching.linear_search
 
 class LinearSearch {
+
+    /**
+     * Performs a linear search on an array of integers to find the index of a specified key.
+     *
+     * @param arr the array to search
+     * @param key the value to search for
+     * @return the index of the key if it is found, or -1 if it is not found
+     */
     fun linearSearch(arr: IntArray, key: Int): Int {
         for (i in arr.indices) {
             if (arr[i] == key) {
                 return i
             }
         }
-
-        // element not found
         return -1
     }
 }
 
 fun main() {
-    val ls = LinearSearch()
+
+    val linearSearch = LinearSearch()
     val arr = intArrayOf(12, 45, 69, 78, 89, 54)
-    val answer = ls.linearSearch(arr, 69)
+    val answer = linearSearch.linearSearch(arr, 69)
     println(answer)
 }
