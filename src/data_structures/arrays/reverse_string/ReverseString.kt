13578ef7c89be00a25c1f5ad6a635ae6b534c021
Asror <a.abdurakhmonoff@gmail.com>
2023-03-07 23:57:03 +0500

    Add comments to reverse string problem sample

--- src/data_structures/arrays/reverse_string/ReverseString.kt
+++ src/data_structures/arrays/reverse_string/ReverseString.kt
@@ -1,17 +1,31 @@
 package data_structures.arrays.reverse_string
 
+/**
+ * This function takes a String parameter and returns the reversed version of the input string.
+ *
+ * @param string String to reverse
+ */
 private fun reverse(string: String): String {
+    // Initialize an empty string
     var result = ""
+    // Iterate through the characters of the input string in reverse order
     for (i in string.length - 1 downTo 0) {
+        // Append each character to the result string to form the reversed string
         result += string[i]
     }
+    // Return the resulting reversed string
     return result
 }
 
-// Note: This method uses built-in function called reversed()
+/**
+ * This function uses Kotlin built-in function called reversed().
+ *
+ * @param string String to reverse
+ */
 private fun reverse2(string: String) = string.reversed()
 
 fun main() {
+
     println(reverse("Hello, World!"))
     println(reverse2("Hello, World!"))
 }
