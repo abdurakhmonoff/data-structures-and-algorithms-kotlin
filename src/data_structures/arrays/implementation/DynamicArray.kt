9d7ccf2809653f7e9de5b689d5fa548131a42d14
Asror <a.abdurakhmonoff@gmail.com>
2023-03-09 23:59:46 +0500

    Add comments to dynamic array implementation

--- src/data_structures/arrays/implementation/DynamicArray.kt
+++ src/data_structures/arrays/implementation/DynamicArray.kt
@@ -1,16 +1,35 @@
 package data_structures.arrays.implementation
 
+/**
+ * Implementation of array data structure that can dynamically resize itself as needed.
+ *
+ * @constructor Creates an empty dynamic array.
+ * @property array The array used to store elements.
+ * @property capacity The current capacity of the array.
+ * @property length The current number of elements in the array.
+ */
 class DynamicArray(
     private var array: Array<String?> = arrayOfNulls(1),
     private var capacity: Int = 1,
     var length: Int = 0
 ) {
-    // gets element at the given index
+
+    /**
+     * Returns the element at the specified index.
+     *
+     * @param index The index of the element to retrieve.
+     * @return The element at the specified index, or "IndexOutOfBounds" if the index is out of range.
+     */
     fun get(index: Int): String? {
         return if (index > -1 && index < length) array[index] else "IndexOutOfBounds"
     }
 
-    // this method adds an element to the end of array
+    /**
+     * Appends the specified element to the end of the array.
+     * If the array is full, its capacity is doubled before the element is added.
+     *
+     * @param string The element to add.
+     */
     fun push(string: String) {
         if (length == capacity) {
             val tempArray = arrayOfNulls<String>(2 * capacity)
@@ -24,7 +43,12 @@ class DynamicArray(
         length++
     }
 
-    // replaces value at given index
+    /**
+     * Replaces the element at the specified index with the specified value.
+     *
+     * @param index The index of the element to replace.
+     * @param value The new value to set.
+     */
     fun replace(index: Int, value: String) {
         if (index > -1 && index < length)
             array[index] = value
@@ -32,14 +56,20 @@ class DynamicArray(
             println("IndexOutOfBounds")
     }
 
-    // removes last item on array
+    /**
+     * Removes the last element from the array.
+     */
     fun pop() {
         if (length > 0) {
             length--
         }
     }
 
-    // deletes element at the given index
+    /**
+     * Removes the element at the specified index from the array.
+     *
+     * @param index The index of the element to remove.
+     */
     fun delete(index: Int) {
         if (index > -1 && index < length) {
             if (index == length - 1) {
@@ -55,7 +85,11 @@ class DynamicArray(
         }
     }
 
-    // gets the array
+    /**
+     * Returns a new array containing all of the elements in this DynamicArray.
+     *
+     * @return An array containing all of the elements in this DynamicArray.
+     */
     fun array(): Array<String?> {
         val tempArr = arrayOfNulls<String>(length)
         for (i in 0 until length) {
@@ -66,19 +100,25 @@ class DynamicArray(
 }
 
 fun main() {
+
     val dynamicArray = DynamicArray()
     dynamicArray.push("apple")
     dynamicArray.push("ball")
     dynamicArray.push("cat")
     dynamicArray.push("dog")
+
     println("Array length: " + dynamicArray.length)
     println("array: " + dynamicArray.array().contentToString())
     println("item at index 2: " + dynamicArray.get(2))
+
     dynamicArray.replace(1, "world")
     println("array: " + dynamicArray.array().contentToString())
+
     dynamicArray.pop()
     println("array: " + dynamicArray.array().contentToString())
+
     dynamicArray.delete(1)
     println("array: " + dynamicArray.array().contentToString())
+
     println("Array length: " + dynamicArray.length)
 }
