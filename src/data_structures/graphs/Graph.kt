ee0bdbd9e38979bf5ebb44e6b41b8e9e44a2d251
Asror <a.abdurakhmonoff@gmail.com>
2023-03-10 21:28:10 +0500

    Add comments to graph implementation

--- src/data_structures/graphs/Graph.kt
+++ src/data_structures/graphs/Graph.kt
@@ -3,17 +3,39 @@ package data_structures.graphs
 import java.util.*
 import kotlin.collections.ArrayList
 
-class Graph(var numberOfNodes: Int = 0, var adjacentList: Hashtable<Int, ArrayList<Int>> = Hashtable()) {
+/**
+ * A Kotlin class that represents an undirected graph using an adjacency list.
+ *
+ * @property numberOfNodes The number of nodes in the graph.
+ * @property adjacentList The adjacency list that stores the connections between nodes.
+ */
+class Graph(
+    var numberOfNodes: Int = 0,
+    var adjacentList: Hashtable<Int, ArrayList<Int>> = Hashtable()
+) {
+
+    /**
+     * Adds a new vertex (node) to the graph.
+     * @param node The node to be added to the graph.
+     */
     fun addVertex(node: Int) {
         adjacentList[node] = ArrayList()
         numberOfNodes++
     }
 
+    /**
+     * Adds an edge between two nodes in the graph.
+     * @param node1 The first node of the edge.
+     * @param node2 The second node of the edge.
+     */
     fun addEdge(node1: Int, node2: Int) {
         adjacentList[node1]?.add(node2)
         adjacentList[node2]?.add(node1)
     }
 
+    /**
+     * Prints out the adjacency list that stores the connections between nodes in the graph.
+     */
     fun showConnections() {
         val keys: Array<Any> = adjacentList.keys.toTypedArray()
         for (key in keys) {
@@ -23,6 +45,7 @@ class Graph(var numberOfNodes: Int = 0, var adjacentList: Hashtable<Int, ArrayLi
 }
 
 fun main() {
+
     val graph = Graph()
     graph.addVertex(5)
     graph.addVertex(54)
