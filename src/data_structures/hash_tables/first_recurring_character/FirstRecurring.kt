8f269c5f44b52f1a8ed5bab747dd37feef6f144d
Asror <a.abdurakhmonoff@gmail.com>
2023-03-12 23:34:36 +0500

    Add comments to first recurring character problem

--- src/data_structures/hash_tables/first_recurring_character/FirstRecurring.kt
+++ src/data_structures/hash_tables/first_recurring_character/FirstRecurring.kt
@@ -1,5 +1,10 @@
 package data_structures.hash_tables.first_recurring_character
 
+/**
+ * Finds the first recurring integer in an array of integers.
+ * @param array an integer array to search for recurring integer
+ * @return The first recurring integer or null if none found
+ */
 private fun firstRecurringCharacter(array: IntArray): Int? {
     val hashSet = HashSet<Int>()
     for (element in array) {
@@ -13,6 +18,7 @@ private fun firstRecurringCharacter(array: IntArray): Int? {
 }
 
 fun main() {
+
     val firstRecurring = firstRecurringCharacter(intArrayOf(2, 5, 5, 2, 3, 5, 1, 2, 4))
     println(firstRecurring)
 }
