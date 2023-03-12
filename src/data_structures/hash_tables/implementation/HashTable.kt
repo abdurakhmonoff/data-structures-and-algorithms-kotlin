57d363b1f91a65a9e0bcf1772ba59586095222b2
Asror <a.abdurakhmonoff@gmail.com>
2023-03-13 00:09:10 +0500

    Add comments to hash table implementation

--- src/data_structures/hash_tables/implementation/HashTable.kt
+++ src/data_structures/hash_tables/implementation/HashTable.kt
@@ -2,10 +2,19 @@ package data_structures.hash_tables.implementation
 
 import java.util.*
 
+/**
+ * A class representing a hash table data structure that stores key-value pairs.
+ * @param size the initial size of the hash table
+ */
 class HashTable(size: Int) {
     private var data: Array<ArrayList<KeyValue>?> = arrayOfNulls<ArrayList<KeyValue>?>(size)
     private var currentLength: Int = 0
 
+    /**
+     * A private hash function that takes a key as input and returns its corresponding hash code.
+     * @param key the key to hash
+     * @return the hash code of the key
+     */
     private fun _hash(key: String): Int {
         var hash = 0
         for (i in key.indices) {
@@ -14,6 +23,11 @@ class HashTable(size: Int) {
         return hash
     }
 
+    /**
+     * A method that inserts a key-value pair into the hash table.
+     * @param key the key of the pair to insert
+     * @param value the value of the pair to insert
+     */
     operator fun set(key: String, value: Int) {
         val address = _hash(key)
         if (data[address] == null) {
@@ -25,6 +39,11 @@ class HashTable(size: Int) {
         data[address]?.add(pair)
     }
 
+    /**
+     * A method that retrieves the value associated with a given key from the hash table.
+     * @param key the key of the pair to retrieve
+     * @return the value associated with the given key or null if the key is not found
+     */
     operator fun get(key: String): Int? {
         val address = _hash(key)
         val bucket = data[address]
@@ -38,6 +57,10 @@ class HashTable(size: Int) {
         return null
     }
 
+    /**
+     * A method that returns an array of all keys in the hash table.
+     * @return an array of all keys in the hash table
+     */
     fun keys(): Array<String?> {
         val bucket = data
         val keysArray = arrayOfNulls<String>(currentLength)
@@ -53,6 +76,7 @@ class HashTable(size: Int) {
 }
 
 fun main() {
+
     val hashTable = HashTable(50)
     hashTable["grapes"] = 1200
     hashTable["apple"] = 1500
--- src/data_structures/hash_tables/implementation/KeyValue.kt
+++ src/data_structures/hash_tables/implementation/KeyValue.kt
@@ -1,3 +1,8 @@
 package data_structures.hash_tables.implementation
 
+/**
+ * A data class representing a key-value pair.
+ * @param key the key of the pair
+ * @param value the value of the pair
+ */
 data class KeyValue(val key: String, val value: Int)
