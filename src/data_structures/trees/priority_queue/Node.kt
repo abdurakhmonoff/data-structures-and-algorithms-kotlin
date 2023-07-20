87295cec8cab9fa575ba07df274c1054cab1c83e
Asror <a.abdurakhmonoff@gmail.com>
2023-07-20 17:04:35 +0500

    Add comments to priority queue implementation

--- src/data_structures/trees/priority_queue/Node.kt
+++ src/data_structures/trees/priority_queue/Node.kt
@@ -1,3 +1,9 @@
 package data_structures.trees.priority_queue
 
-class Node(var value: String, var priority: Int)
+/**
+ * Represents a node in the priority queue.
+ *
+ * @param value The value to be stored in the node.
+ * @param priority The priority of the node.
+ */
+class Node(val value: String, val priority: Int)
--- src/data_structures/trees/priority_queue/PriorityQueue.kt
+++ src/data_structures/trees/priority_queue/PriorityQueue.kt
@@ -1,8 +1,19 @@
 package data_structures.trees.priority_queue
 
+/**
+ * Simple implementation of a priority queue data structure.
+ *
+ * @property queueArray ArrayList that holds the nodes in the priority queue.
+ */
 class PriorityQueue {
     private val queueArray = ArrayList<Node>()
 
+    /**
+     * Adds a new node with the given value and priority to the priority queue.
+     *
+     * @param value The value to be added to the priority queue.
+     * @param priority The priority of the value. Higher values represent higher priority.
+     */
     fun enqueue(value: String?, priority: Int) {
         val newNode = Node(value!!, priority)
         var contain = false
@@ -18,6 +29,11 @@ class PriorityQueue {
         }
     }
 
+    /**
+     * Removes and returns the value of the node with the highest priority from the priority queue.
+     *
+     * @return The value of the node with the highest priority, or "empty array" if the queue is empty.
+     */
     fun dequeue(): String {
         if (queueArray.isEmpty()) {
             return "empty array"
@@ -27,21 +43,39 @@ class PriorityQueue {
         return toRemove.value
     }
 
+    /**
+     * Returns the node with the highest priority in the priority queue without removing it.
+     *
+     * @return The node with the highest priority, or null if the queue is empty.
+     */
     fun front(): Node? {
         return if (queueArray.isNotEmpty()) {
             queueArray[0]
         } else null
     }
 
+    /**
+     * Returns the node with the lowest priority in the priority queue without removing it.
+     *
+     * @return The node with the lowest priority, or null if the queue is empty.
+     */
     fun rear(): Node? {
         return if (queueArray.isNotEmpty()) {
             queueArray[queueArray.size - 1]
         } else null
     }
 
+    /**
+     * Property that indicates whether the priority queue is empty or not.
+     */
     val isEmpty: Boolean
         get() = queueArray.isEmpty()
 
+    /**
+     * Returns an array containing the values of all nodes in the priority queue.
+     *
+     * @return An array of String? containing the values of all nodes in the priority queue.
+     */
     fun printQueue(): Array<String?> {
         val array = arrayOfNulls<String>(queueArray.size)
         for (i in array.indices) {
@@ -52,6 +86,7 @@ class PriorityQueue {
 }
 
 fun main() {
+
     val queue = PriorityQueue()
     queue.enqueue("piyush", 3)
     queue.enqueue("rohan", 1)
