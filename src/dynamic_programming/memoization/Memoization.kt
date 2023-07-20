cd9746d02b34b54dd0cf8c8dacc2fb91d2a3cc07
Asror <a.abdurakhmonoff@gmail.com>
2023-07-20 17:14:26 +0500

    Add comments to a sample demonstrating the concept of memoization

--- src/dynamic_programming/memoization/Memoization.kt
+++ src/dynamic_programming/memoization/Memoization.kt
@@ -2,12 +2,28 @@ package dynamic_programming.memoization
 
 import java.util.*
 
+/**
+ * A class demonstrating the concept of memoization in Kotlin.
+ */
 class Memoization {
+
+    /**
+     * Calculates the sum of the given integer 'n' and 80 directly without memoization.
+     *
+     * @param n The integer to add to 80.
+     * @return The sum of 'n' and 80.
+     */
     fun addTo80(n: Int): Int {
         println("long time")
         return n + 80
     }
 
+    /**
+     * Calculates the sum of the given integer 'n' and 80 using memoization to avoid redundant calculations.
+     *
+     * @param n The integer to add to 80.
+     * @return The sum of 'n' and 80, retrieved from the cache if previously calculated.
+     */
     fun memoizeAddTo80(n: Int): Int {
         if (!cache.containsKey(n)) {
             println("long time")
@@ -17,11 +33,13 @@ class Memoization {
     }
 
     companion object {
+        // HashMap to store previously calculated results for memoization (cache).
         var cache = HashMap<Int, Int>()
     }
 }
 
 fun main() {
+
     val memo = Memoization()
     println("addTo80: " + memo.addTo80(5))
     println("addTo80: " + memo.addTo80(5))
